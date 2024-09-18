c592fba3cc1a9545b7f9bf37a34ccd3c027eb388
xeenogoat <07kamilos@gmail.com>
2024-09-18 14:44:04 +0200

    lekcja

--- src/Osoba.java
+++ src/Osoba.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Osoba {
+public class Osoba {
+    public String imię;
+    public String nazwisko;
+    public int wiek;
+
+    public Osoba(String imię, String nazwisko, int wiek) {
+        this.imię = imię;
+        this.nazwisko = nazwisko;
+        this.wiek = wiek;
+    }
 }
